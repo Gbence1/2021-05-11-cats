5ecff13cf55e8e0e9f7cda969f841c6448673df1
Bence Gallai <bencegallai460@gmail.com>
2021-05-18 17:39:32 +0200

    full js overhaul, replaced adapted code w/ own

--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
-// Code adapted from
-// https://www.w3schools.com/howto/howto_js_tab_img_gallery.asp
-// with modifications
+//Close image
 let cross = document.querySelector(".cross");
 
 cross.addEventListener("click", close);
 
-function expand(img) {
-	let expandImg = document.querySelector(".expandImg");
-	let imgText = document.querySelector(".imgText");
-	expandImg.src = img.src;
-	imgText.innerHTML = img.alt;
-	document.querySelector(".expand").style.display = "block";
-}
-
 function close() {
 	document.querySelector(".expand").style.display = "none";
+}
+
+//Expand image
+
+let img = document.querySelectorAll(".img");
+
+for (let i = 0; i < img.length; i++) {
+	img[i].addEventListener("click", expand);
+}
+
+function expand(j) {
+	let image = j.target;
+	let expandImg = document.querySelector(".expandImg");
+	let imgText = document.querySelector(".imgText");
+	expandImg.src = image.src;
+	imgText.innerHTML = image.alt;
+	document.querySelector(".expand").style.display = "block";
 }
